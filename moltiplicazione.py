eb216c9dc462445abb8939bdd02bea0d796b136b
LolloMiglio <lorenzo.migliore@iismajoranamoncalieri.edu.it>
2024-10-10 11:57:39 +0000

    oke

---
+++ moltiplicazione.py
@@ -0,0 +1,11 @@
+"""
+programma che legge due numeri in input 
+e li moltiplica solo se sono entrambi positivi
+"""
+
+x = input("Inserisci il primo numero (positivo): ")
+y = input("Inserisci il secondo numero (positivo): ")
+
+x = int(x)
+y = int(y)
+
